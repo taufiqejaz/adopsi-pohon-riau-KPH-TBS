a72953b6403f30847e80da1c7ff6e5708dab5999
Taufiq Ejaz Ahmad <79326397+taufiqejaz@users.noreply.github.com>
2021-11-15 21:08:30 +0700

    re-upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+// Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
+// or a Google Sheets URL (File > Publish to the Web > CSV )
+const dataLocation = 'data/places.csv';
+
+// Map's initial center, and zoom level
+const mapCenter = [0.7, 102.309];
+const mapZoom = 9;
+
+// Marker icon height and width
+const iconHeight = 30;
+const iconWidth = 30;
+
+//Icon for pohon muda and pohon dewasa
+const pohonDewasa =  'logo/pohon_dewasa.svg'
+const pohonMuda = 'logo/tree-5202.svg'
